9dec8978c6dbb885d82efba7efbe7d7ccc109a4f
Christof Sy <sy.necu@gmail.com>
2020-03-10 07:46:41 +0100

    added tool description

--- adventure/src/App.js
+++ adventure/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Location from './components/Location';
 import Navigation from './components/Navigation';
+import { toolDescription } from './helper/ToolDescription';
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
+      <p className="toolDescription">{toolDescription}</p>
       <div className="App">
         <Location></Location>
       </div>
---
+++ adventure/src/helper/ToolDescription.js
@@ -0,0 +1,2 @@
+export const toolDescription = "This tool is meant to be used alongside storytelling by the DM or by different people of the PCs. "
+    + "Write additional stuff about the locations, people, etc. in the notes sections."
